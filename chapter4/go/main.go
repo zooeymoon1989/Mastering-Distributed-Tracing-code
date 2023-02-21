1b970eeb41760412b187bda5f0fdc00204652593
zooeymoon <kimmi-moon@163.com>
2023-02-22 05:32:02 +0800

    add tracer span named say-hello

--- chapter4/go/main.go
+++ chapter4/go/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"Mastering-Distributed-Tracing-code/lib/tracing"
 	"Mastering-Distributed-Tracing-code/people"
+	opentracing "github.com/opentracing/opentracing-go"
 	"log"
 	"net/http"
 	"strings"
 )
 
 var repo *people.Repository
+var tracer opentracing.Tracer
 
 func main() {
 	repo = people.NewRepository()
 	defer repo.Close()
+
+	tr, closer := tracing.Init("go-2-hello")
+	defer closer.Close()
+	tracer = tr
 
 	http.HandleFunc("/sayHello/", handleSayHello)
 
@@ -20,6 +27,8 @@ func main() {
 }
 
 func handleSayHello(w http.ResponseWriter, r *http.Request) {
+	span := tracer.StartSpan("say-hello")
+	defer span.Finish()
 	name := strings.TrimPrefix(r.URL.Path, "/sayHello/")
 	greeting, err := SayHello(name)
 	if err != nil {
